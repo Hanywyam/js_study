2db57298ac8ea1ef4198470a254a30ff95ebbd59
hanywyam <hellow522@naver.com>
2023-10-23 17:50:02 +0900

    231023 js study

---
+++ 231023/functionEx7.js
@@ -0,0 +1,14 @@
+/* 
+css VS js 스타일 값 작성
+
+background-color | backgroundColor
+margin-right | marginRight
+padding-top | paddingTop
+
+js에서는
+띄어쓰기 없이 대문자로 작성
+*/
+function orange() {
+  document.body.style.backgroundColor = "orange";
+}
+const tomato = () => (document.body.style.backgroundColor = "tomato");
